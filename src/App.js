bad87f7dffee86223e20dde4dffe76c858b59700
Joash Nyabuti <nyabutijoash@yahoo.com>
2021-02-24 11:55:21 -0600

    first filter app commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import data from './data.js'
+import { useState } from "react";
 
 function App() {
+  const [filter, setfilter] = useState("");
+  const [display, setdisplay] = useState(data)
+
+  const handleChange=(e)=>{
+    // console.log(e.target.value);
+    setfilter(e.target.value);
+
+    const newdata=display.filter((data) => data.id.includes(filter))
+    console.log(newdata)
+    setdisplay(newdata)
+
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React filter app</h1>
+      <input type="text" value={filter} onChange={handleChange}/>
+          <table>
+            <thead>
+              <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>mini amount in $.</th>
+              </tr>
+              
+            </thead>
+            
+            <tbody>
+      {display.map((data)=>{
+        return(
+              <tr key={data.id}>
+                <td>{data.id}</td>
+                <td>{ data.name } </td>
+                <td>{data.min_size}</td>
+              </tr>
+              )
+            })}
+            </tbody>
+          </table>
+         
     </div>
   );
 }
